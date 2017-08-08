5d382e659932f6bd2595e117f597f8a56c1b2604
mck <mick@thelastpickle.com>
2017-08-08 22:40:11 +1000

    Add methods to JmxProxy so know which datacenters each host is in.
    If `allowUnreachableNodes=true` then it's only required that we have metrics to all hosts within the coordinator's datacenter.
    Taking this approach allows us to remove the cached host metrics in the Cassandra backend.

--- src/main/java/com/spotify/reaper/service/RepairRunner.java
+++ src/main/java/com/spotify/reaper/service/RepairRunner.java
@@ -43,6 +43,7 @@ import com.spotify.reaper.core.RepairUnit;
 import com.spotify.reaper.storage.IDistributedStorage;
 import java.util.Collection;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class RepairRunner implements Runnable {
 
@@ -321,9 +322,9 @@ public class RepairRunner implements Runnable {
 
     List<String> potentialCoordinators;
     if(!repairUnit.getIncrementalRepair()) {
-    	// full repair    
+    	    // full repair
 	    try {
-	      potentialCoordinators = jmxConnection.tokenRangeToEndpoint(keyspace, tokenRange);
+	       potentialCoordinators = jmxConnection.tokenRangeToEndpoint(keyspace, tokenRange);
 	    } catch (RuntimeException e) {
 	      LOG.warn("Couldn't get token ranges from coordinator: #{}", e);
 	      return true;
@@ -344,10 +345,8 @@ public class RepairRunner implements Runnable {
 	      }
 	      return false;
 	    }
-    } 
-    else {
-    	potentialCoordinators 
-                = Arrays.asList(context.storage.getRepairSegment(repairRunId, segmentId).get().getCoordinatorHost());
+    } else {
+    	    potentialCoordinators = Arrays.asList(context.storage.getRepairSegment(repairRunId, segmentId).get().getCoordinatorHost());
     }
 
     try {
@@ -366,11 +365,11 @@ public class RepairRunner implements Runnable {
           @Override
           public void onFailure(Throwable t) {
             currentlyRunningSegments.set(rangeIndex, null);
-            LOG.error("Executing SegmentRunner failed: {}", t.getMessage());
+            LOG.error("Executing SegmentRunner failed", t);
           }
         });
     } catch (ReaperException ex) {
-        LOG.error("Executing SegmentRunner failed: {}", ex.getMessage());
+        LOG.error("Executing SegmentRunner failed", ex);
     }
 
     return true;
